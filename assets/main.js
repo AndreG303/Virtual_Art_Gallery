245a7c743bb204aec72c128c1f158debf0528fd0
nlamonaco86 <nlamonaco86@gmail.com>
2020-07-10 13:47:39 -0400

    added render buttons function

---
+++ assets/main.js
@@ -0,0 +1,24 @@
+// Default Artist Buttons
+var artists = ["Leonardo DaVinci", "Michaelangelo", "Pablo Picasso", "Monet"]
+
+//Function that adds default artists to the page on load
+function renderButtons() {
+
+    // Deletes the artists prior to adding new movies
+    $("#artistName").empty();
+    // Loops through the array of artists
+    for (var i = 0; i < artists.length; i++) {
+
+      // Then dynamicaly generates buttons for each artist in the array
+      var a = $("<button>");
+      // Adds a class of artist to our button
+      a.addClass("artist");
+      // Added a data-attribute
+      a.attr("data-name", artists[i]);
+      // Provided the initial button text
+      a.text(artists[i]);
+      // Added the button to the artists button div
+      $("#artistName").append(a);
+    }
+  };
+
